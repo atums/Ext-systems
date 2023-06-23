a26ce4bf69419d5847075e9fc1734158b03a6d65
tums-MSI\tums <alex.tums@gmail.com>
2023-06-23 15:20:12 +0400

    HTTP Client Server and Multithread

--- network/src/main/java/com/apys/learning/net/Client.java
+++ network/src/main/java/com/apys/learning/net/Client.java
@@ -2,30 +2,45 @@ package com.apys.learning.net;
 
 import java.io.*;
 import java.net.Socket;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        sendRequest();
+        for(int i = 0; i < 500; i++) {
+            SimpleClass sc = new SimpleClass();
+            sc.start();
+        }
     }
 
-    private static void sendRequest() throws IOException {
-        Socket socket = new Socket("127.0.0.1", 25225);
+}
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+class SimpleClass extends Thread {
+    @Override
+    public void run() {
+        try {
+            System.out.println("Started : " + LocalDateTime.now());
+            Socket socket = new Socket("127.0.0.1", 25225);
 
-        String sb = "Alex";
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-        bw.write(sb);
-        bw.newLine();
-        bw.flush();
+            String sb = "Alex";
 
-        String answer = br.readLine();
-        System.out.println("Client got answer from server : " + answer);
+            bw.write(sb);
+            bw.newLine();
+            bw.flush();
 
-        br.close();
-        bw.close();
+            String answer = br.readLine();
+            System.out.println("Client got answer from server : " + answer);
 
-        socket.close();
+            br.close();
+            bw.close();
+
+            socket.close();
+            System.out.println("Finished : " + LocalDateTime.now());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- network/src/main/java/com/apys/learning/net/Server.java
+++ network/src/main/java/com/apys/learning/net/Server.java
@@ -5,34 +5,51 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket socket = new ServerSocket(25225);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket socket = new ServerSocket(25225, 2000);
 
         System.out.println("Server is started");
 
         while(true) {
             Socket accept = socket.accept();
-            handleRequest(accept);
+            new SimpleServer(accept).start();
         }
     }
+}
 
-    private static void handleRequest(Socket accept) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(accept.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(accept.getOutputStream()));
+class SimpleServer extends Thread {
+    private Socket accept;
 
-        StringBuilder sb = new StringBuilder("Hello, ");
-        String str = br.readLine();
-        sb.append(str);
+    public SimpleServer(Socket accept) {
+        this.accept = accept;
+    }
+    @Override
+    public void run() {
+        handleRequest();
+    }
+    private void handleRequest() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(accept.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(accept.getOutputStream()));
 
-        System.out.println("Server got string : " + str);
+            StringBuilder sb = new StringBuilder("Hello, ");
+            String str = br.readLine();
+            System.out.println("Server got string : " + str);
+            Thread.sleep(2000);
+            sb.append(str);
 
-        bw.write(sb.toString());
-        bw.newLine();
-        bw.flush();
+            bw.write(sb.toString());
+            bw.newLine();
+            bw.flush();
 
-        br.close();
-        bw.close();
+            br.close();
+            bw.close();
 
-        accept.close();
+            accept.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
     }
 }

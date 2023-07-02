a844ed8792b773b72a2f8b50651d4d9d0923f7fe
tums-MSI\tums <alex.tums@gmail.com>
2023-07-02 21:13:58 +0400

    Hibernate/JPA + persistence.xml + Initializatin + WORK

--- register-office/src/main/java/com/apys/learning/ext/register_offece/manager/PersonManager.java
+++ register-office/src/main/java/com/apys/learning/ext/register_offece/manager/PersonManager.java
@@ -8,48 +8,108 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.io.Serializable;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 
 
 public class PersonManager {
     public static void main(String[] args) {
 
+        sessionExample();
+
+        jpaExample();
+    }
+
+    private static void jpaExample() {
+        // Аналог SessionFactory
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistence");
+
+        // Аналог Session
+        EntityManager em = emf.createEntityManager();
+
+        // Начать транзакцию
+        em.getTransaction().begin();
+
+        // Создаем объекты для помещения в DB
+        Person person = new Person();
+        person.setFirstName("Marina");
+        person.setLastName("Tums");
+
+        // Аналог save и saveOfUpdate
+        em.persist(person);
+
+        // Аналог возвращаемого значения
+        System.out.println("JPA Id = " + person.getPersonId());
+
+        // Аналог Коммита
+        em.getTransaction().commit();
+
+        // Закрываем сесиию
+        em.close();
+
+        // Снова открываем на тойже фабрике
+        em = emf.createEntityManager();
+
+        // Аналог запроса только пишем на JPAQL
+        List<Person> from_person = em.createQuery("FROM Person", Person.class).getResultList();
+        from_person.forEach(p -> System.out.println(p));
+
+        // Закрываем сесиию
+        em.close();
+    }
+
+    private static void sessionExample() {
+        // Создаем Фактори
         SessionFactory sf = buildSessionFactory();
 
-        Session session = sf.openSession(); //Открытие сессии
+        //Открытие сессии
+        Session session = sf.openSession();
 
-        session.getTransaction().begin(); // Позволяет внести изменения открыть транзакцию
+        // Позволяет внести изменения открыть транзакцию
+        session.getTransaction().begin();
 
+        // Создаем объекты для помещения в DB
         Person person = new Person();
         person.setFirstName("Alex");
         person.setLastName("Tums");
 
-        Long id = (Long) session.save(person); // Хотим сохранить в DB - возвращиет id primary key
+        // Хотим сохранить в DB - возвращиет id primary key
+        Long id = (Long) session.save(person);
 
-       session.getTransaction().commit(); // Внести изменения в DB
+        // Внести изменения в DB
+        session.getTransaction().commit();
 //        session.getTransaction().rollback(); // Откатить из менения
-
-        session.close();
         System.out.println("Id = " + id);
 
-        session = sf.openSession();
-
-        Person person1 = session.get(Person.class, id);// Получить запись из DB по id
-        System.out.println("Person #" + id + " " + person1.toString());
-
+        // Закрываем сессию
         session.close();
 
+        // Снова открываем на тойже фабрике
         session = sf.openSession();
 
-        List<Person> personList = session.createQuery("FROM Person", Person.class).list(); // Запрос на языке HQL (вызвать все записи)
+        // Получить запись из DB по id
+        Person person1 = session.get(Person.class, id);
+        System.out.println("Person #" + id + " " + person1.toString());
+
+        // Закрываем сессию
+        session.close();
+
+        // Снова открываем на тойже фабрике
+        session = sf.openSession();
+
+        // Запрос на языке HQL (вызвать все записи)
+        List<Person> personList = session.createQuery("FROM Person", Person.class).list();
 
         for(Person p : personList) {
             System.out.println(p.toString());
         }
 
+        // Закрываем сессию
         session.close();
     }
+
     private static SessionFactory buildSessionFactory() {
 
         try {
